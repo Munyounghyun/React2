55c98dab081de1388c62fece8ec241ca220fe273
Munyounghyun <fldh3369@naver.com>
2022-01-25 16:32:24 +0900

    ignore파일로 비밀정보파일 생성

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require("express"); //express 모듈 가져옴
 const app = express();
 const port = 5000;
 
-//mongodb+srv://fldh3369:cldrn5623@cluster0.3rvhs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+//application/x--www-form-urlencoded 를 분석해서 가져올수있게함
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json json타입을 분석해서 가져올수있게함
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://fldh3369:cldrn5623@cluster0.3rvhs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    config.mongoURI
     // {
     //   useNewUrlParser: true,
     //   useUnifiedTopology: true,
@@ -19,7 +28,20 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World! 안녕하세요");
+  res.send("새해복 많이받으세요");
+});
+
+app.post("/register", (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
