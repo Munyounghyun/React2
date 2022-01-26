0e70d97352bf90d9f5a9aac7331367b5089a2173
Munyounghyun <fldh3369@naver.com>
2022-01-26 17:45:52 +0900

    두번째 프로젝트 2

---
+++ client/src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+      A <Switch> looks through all its children <Route>
+      elements and renders the first one whose path
+      matches the current URL. Use a <Switch> any time
+      you have multiple routes, but you want only one
+      of them to render at a time
+    */}
+        <Routes>
+          <Route exact={true} path={"/"} element={<Home />} />
+
+          <Route exact={true} path="/about" element={<About />} />
+
+          <Route exact={true} path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require("express"); //express 모듈 가져옴
 const app = express();
 const port = 5000;
+const { auth } = require("./middleware/auth");
 
 const bodyParser = require("body-parser");
-
 const config = require("./config/key");
+const cookieParser = require("cookie-parser");
 
 const { User } = require("./models/User");
 
@@ -12,6 +13,7 @@ const { User } = require("./models/User");
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/json json타입을 분석해서 가져올수있게함
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 const { request } = require("express");
@@ -33,7 +35,7 @@ app.get("/", (req, res) => {
 });
 
 //회원가입
-app.post("/register", (req, res) => {
+app.post("/api/user/register", (req, res) => {
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다.
   const user = new User(req.body);
@@ -49,7 +51,7 @@ app.post("/register", (req, res) => {
 //로그인
 app.post("/login", (req, res) => {
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
-  User.findOne({ email: request.body.email }, (err, user) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
       return res.json({
         loginSuccess: false,
@@ -64,7 +66,38 @@ app.post("/login", (req, res) => {
           message: "비밀번호가 틀렸습니다.",
         });
       //비밀번호까지 맞다면 토큰을 생성하기
-      user.generateToken((err, user) => {});
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        //토큰을 저장한다. 어디에? 쿠키,로컬스토리지 등등
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  //여기 까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 ture 라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    //role 0 일반유저 , 0이아닌 나머지는 관리자
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
     });
   });
 });
